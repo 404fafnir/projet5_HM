a63526346105b73966fff6aa30fd2bf93274437a
404Fafnir <heikocarlier@gmail.com>
2022-11-21 09:52:46 +0100

    First Commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.carousel').carousel();
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+    });
+    $('.modal').modal();
+});
+
+
