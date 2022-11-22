acd25ce098f56236374aea4706adfffc7b08d46d
AegesArenate <maxsenetaire@hotmail.com>
2022-11-22 12:13:56 +0100

    Modification présentation et fin de cours

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
+    $('.parallax').parallax();
     $('.carousel').carousel();
     $('.carousel.carousel-slider').carousel({
         fullWidth: true
