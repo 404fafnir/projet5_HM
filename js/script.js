dd83f4b3ccb4eef0caecd6a70c5ae7a3af65493d
404Fafnir <heikocarlier@gmail.com>
2022-11-23 14:35:43 +0100

    adding ee saddy and happy
    Co-authored-by: AegesArenate <AegesArenate@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -13,7 +13,29 @@ $(document).ready(function(){
         $('.carousel').carousel('prev');
     });
     $('.modal').modal();
+
+
 });
+
+var typedword = '';
+var theword = 'saddy';
+var thesecondword = 'happy'
+
+window.addEventListener('keypress',
+    function (e) {
+       typedword += String.fromCharCode(e.which);
+        if (typedword.length > theword.length){
+            typedword = typedword.slice(1);
+        }
+        if (typedword == theword){
+            $('body').toggleClass('filter', true);
+        }
+        if (typedword == thesecondword){
+            $('body').toggleClass('filter', false);
+        }
+    }
+);
+
 
 /*
 setInterval(function(){
